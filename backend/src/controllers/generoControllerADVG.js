19e5c877b0c6b3819d493022f7e080439ad65a25
advg-2005 <advguwu.09.02.2005@gmail.com>
2025-05-04 00:27:37 -0500

    Controlador completo para el modelo de la tabla genero

---
+++ backend/src/controllers/generoControllerADVG.js
@@ -0,0 +1,88 @@
+import { PrismaClient } from "@prisma/client";
+const prismaADVG = new PrismaClient()
+
+export const postADVG = async (req,res) => {
+    try {
+        const {id,nombre}=req.body
+        const sql = await prismaADVG.genero.create({
+            data: {
+                id : id,
+                nombre : nombre
+            }
+        })
+        if(sql){
+            return res.status(201).json({msg : "El genero fue creado correctamente",data:sql})
+        }
+    }
+    catch(error){
+        console.error(error)
+        return res.status(500).json({"msg" : "Error en el servidor"})
+    }
+}
+export const getADVG = async (req,res) => {
+    try{
+        const sql = await prismaADVG.genero.findMany()
+        if (sql){
+            return res.status(200).json(sql)
+        }
+    }catch(error){
+        console.error(error)
+        return res.status(500).json({msg : "Error en el servidor"})
+    }
+}
+
+export const getOneADVG = async (req,res) => {
+    try{
+        const {id}=req.params
+        const sql = await prismaADVG.genero.findUnique({
+            where : {id : parseInt(id)},
+        })
+
+        if (sql){
+            return res.status(200).json(sql)
+        }
+        return res.status(404).json({msg : "No se encontro el ID"})
+
+    }catch(error){
+        console.error(error)
+        return res.status(500).json({msg : "Error en el servidor"})
+    }
+}
+
+export const putADVG = async (req,res) => {
+    try{
+        const {id} = req.params
+        const sql = await prismaADVG.genero.update({
+            where : {id : parseInt(id)},
+            data : req.body
+        })
+
+        if (sql) {
+            return res.status(200).json({msg : "Genero actualizado correctamente",sql})
+        }
+    }catch(error){
+        console.error(error)
+        if (error.code == "P2025"){
+            return res.status(404).json({msg : "No se encontro el ID"})
+        }
+        return res.status(500).json({msg : "Error en el servidor"})
+    }
+}
+
+export const deleteADVG = async (req,res) => {
+    try{
+        const {id} = req.params
+        const sql = await prismaADVG.genero.delete({
+            where : {id : parseInt(id)}
+        })
+        if (sql) {
+            return res.status(200).json({msg : "Genero eliminado correctamente"})
+        }
+    }catch(error){
+        console.error(error);
+        if (error.code == "P2025"){
+            return res.status(404).json({msg : "No se encontro el ID"})
+        }
+        return res.status(500).json({msg : "Error en el servidor"})
+    }
+}
